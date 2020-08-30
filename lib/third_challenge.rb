71453e02ffad5e3faffcd7aee24d42d78e8aa2db
Justice Payne <spikepayne@yahoo.com>
2020-08-30 22:13:33 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,13 +4,13 @@ def third_challenge
       :patriarch => {name: "Lord Montague" age:"53"}
       :matriarch => {name: "lady Montague" age:"54"}
       :hero => {name: "Romeo" age: "15" status: "alive"}
-      :hero_friends => [{:name "Benvolio" Age: "17" Attiude: "worried"} {:Name "Mercutio" Age: "18" Attiude: "hot-headed"]
+      :hero_friends => [{:name "Benvolio" age: "17" attiude: "worried"} {:Name "Mercutio" age: "18" attiude: "hot-headed"]
    },
    :capulet => {
       :patriarch => {name: "Lord Capulet" age: "50"}
       :matriarch => {name: "Lady Capulet" age: "51"}
       :heroine => {name: "Juliet" age: "15" status: "alive"}
-      :heroine_friends => [{:name "Steven" age: "30" Attiude: "confused"} {:name "Nurse" Age: "44" Attiude: "worried"]
+      :heroine_friends => [{:name "Steven" age: "30" attiude: "confused"} {:name "Nurse" age: "44" attiude: "worried"
    }
   }
 end
